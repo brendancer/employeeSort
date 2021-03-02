2f9c679781a21475844054d42cc67c54601a360f
Brenda Goodwin <brendanced@gmail.com>
2021-03-02 11:53:22 -0700

    setting up table

--- employee-sort/src/App.js
+++ employee-sort/src/App.js
@@ -1,59 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import API from './utils/API';
-import Table from './components/Table';
-import EmployeeRow from './components/EmployeeRow';
-import Search from './components/Search';
+import React, { useState, useEffect } from "react";
+import API from "./utils/API";
+import Table from "./components/Table";
+// import EmployeeRow from "./components/EmployeeRow";
+// import Search from "./components/Search";
 
 function App() {
-
   const [empArr, setEmpArr] = useState([]);
   const [empSortArr, setEmpSortArr] = useState([]);
-  const [sortType, setSortType] = useState('name');
+  const [sortType, setSortType] = useState("name");
   const [sortDesc, setSortDesc] = useState(false);
-  const [searchFilter, setSearchFilter] = useState('');
+  const [searchFilter, setSearchFilter] = useState("");
 
   useEffect(() => {
-
-    API.search()
-      .then(res => {
-        setEmpArr(res.data.results);
-        setEmpSortArr(res.data.results);
-      })
+    API.search().then((res) => {
+      setEmpArr(res.data.results);
+      setEmpSortArr(res.data.results);
+    });
   }, []);
 
   useEffect(() => {
     const sortUp = sortDesc ? 1 : -1;
     const sortDown = sortDesc ? -1 : 1;
-    let arr = []
+    let arr = [];
     switch (sortType) {
-      case 'picture[thumbnail]':
-        arr = [...empSortArr].sort((a, b) => (a.picture.thumbnail < b.picture.thumbnail) ? sortUp : sortDown);
+      case "picture[thumbnail]":
+        arr = [...empSortArr].sort((a, b) =>
+          a.picture.thumbnail < b.picture.thumbnail ? sortUp : sortDown
+        );
         break;
-      case 'name[first]':
-        arr = [...empSortArr].sort((a, b) => (a.name.first < b.name.first) ? sortUp : sortDown);
+      case "name[first]":
+        arr = [...empSortArr].sort((a, b) =>
+          a.name.first < b.name.first ? sortUp : sortDown
+        );
         break;
-      case 'dob[date]':
-        arr = [...empSortArr].sort((a, b) => (a.dob.date < b.dob.date) ? sortUp : sortDown);
+      case "dob[date]":
+        arr = [...empSortArr].sort((a, b) =>
+          a.dob.date < b.dob.date ? sortUp : sortDown
+        );
         break;
       default:
-        arr = [...empSortArr].sort((a, b) => (a[sortType] < b[sortType]) ? sortUp : sortDown);
+        arr = [...empSortArr].sort((a, b) =>
+          a[sortType] < b[sortType] ? sortUp : sortDown
+        );
         break;
     }
     arr = [...empSortArr].filter((emp) => {
-      return (emp.name.first.includes(searchFilter) || emp.name.last.includes(searchFilter) || emp.phone.includes(searchFilter) || emp.email.includes(searchFilter) || emp.dob.date.includes(searchFilter));
+      return (
+        emp.name.first.includes(searchFilter) ||
+        emp.name.last.includes(searchFilter) ||
+        emp.phone.includes(searchFilter) ||
+        emp.email.includes(searchFilter) ||
+        emp.dob.date.includes(searchFilter)
+      );
     });
     setEmpSortArr(arr);
   }, [sortType, sortDesc, searchFilter]);
-
-  
-//   return (
-//     <div className="App">
-//       <>
-//         <Table />
-//         <EmployeeList />
-//       </>
-//     </div>
-//   );
-// }
-
+}
 export default App;
--- employee-sort/src/components/Table.js
+++ employee-sort/src/components/Table.js
@@ -1,46 +1,48 @@
-// import React from "react";
+import React from "react";
 
-// class Table extends React.Component {
-//   renderTableData() {
-//     return this.state.employees.map((employee, index) => {
-//       const { id, name, gender, city, postcode, email, registered } = employee;
-//       console
-//       return (
-//         <>
-//           <tr>
-//             <th>Employee Id</th>
-//             <th>Name</th>
-//             <th>Gender</th>
-//             <th>City</th>
-//             <th>Zip Code</th>
-//             <th>Email</th>
-//             <th>Date of Employ</th>
-//           </tr>
-
-//           <tr key={id}>
-//             <td>{id}</td>
-//             <td>{name}</td>
-//             <td>{gender}</td>
-//             <td>{city}</td>
-//             <td>{postcode}</td>
-//             <td>{email}</td>
-//             <td>{registered}</td>
-//           </tr>
-//         </>
-//       );
-//     });
-//   }
-
-//   //   render() {
-//   //     return (
-//   //       <ul>
-//   //         {this.props.employee.map((employee) => (
-//   //           <EmployeeData key={employee.id} employees={employee} />
-//   //         ))}
-//   //       </ul>
-//   //     );
-//   //   }
-//   // }
-// }
-
-// export default Table;
+function Table(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">
+            <button
+              onClick={() => props.handleSort("picture[thumbnail]")}
+              className="btn"
+            >
+              Picture
+            </button>
+          </th>
+          <th scope="col">
+            <button
+              onClick={() => props.handleSort("name[first]")}
+              className="btn"
+            >
+              Name
+            </button>
+          </th>
+          <th scope="col">
+            <button onClick={() => props.handleSort("phone")} className="btn">
+              Phone
+            </button>
+          </th>
+          <th scope="col">
+            <button onClick={() => props.handleSort("email")} className="btn">
+              email
+            </button>
+          </th>
+          <th scope="col">
+            <button
+              onClick={() => props.handleSort("dob[date]")}
+              className="btn"
+            >
+              Birthday
+            </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>{props.children}</tbody>
+    </table>
+  );
+}
+export default Table;
